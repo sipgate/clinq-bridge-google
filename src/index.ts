5b930a436f734fe94d783394c75ebaf3fcb87af7
Felix Gladisch <felix@felixgladisch.de>
2018-07-23 14:49:47 +0200

    feat: new clinq bridge library

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
-import { Contact, CrmAdapter, CrmConfig, start } from "clinq-crm-bridge";
+import { Adapter, Config, Contact, start } from "@clinq/bridge";
 import { Request } from "express";
 import { getGoogleContacts, getOAuth2Client, getOAuth2RedirectUrl } from "./util";
 
-class GoogleContactsAdapter implements CrmAdapter {
-	public async getContacts(config: CrmConfig): Promise<Contact[]> {
+class GoogleContactsAdapter implements Adapter {
+	public async getContacts(config: Config): Promise<Contact[]> {
 		const [access_token, refresh_token] = config.apiKey.split(":");
 		const client = getOAuth2Client();
 		client.setCredentials({
 			access_token,
 			refresh_token
 		});
-		const { credentials } = await client.refreshAccessToken();
-		const contacts = await getGoogleContacts(credentials.access_token);
+		await client.refreshAccessToken();
+		const contacts = await getGoogleContacts(client);
 		return contacts;
 	}
 
@@ -19,10 +19,12 @@ class GoogleContactsAdapter implements CrmAdapter {
 		return getOAuth2RedirectUrl();
 	}
 
-	public async handleOAuth2Callback(req: Request): Promise<CrmConfig> {
+	public async handleOAuth2Callback(req: Request): Promise<Config> {
 		const { code } = req.query;
 		const client = getOAuth2Client();
-		const { tokens: { access_token, refresh_token } } = await client.getToken(code);
+		const {
+			tokens: { access_token, refresh_token }
+		} = await client.getToken(code);
 		return {
 			apiKey: `${access_token}:${refresh_token}`,
 			apiUrl: ""
--- src/util/google.ts
+++ src/util/google.ts
@@ -1,15 +1,25 @@
-import axios from "axios";
-import { Contact } from "clinq-crm-bridge";
+import { Contact } from "@clinq/bridge";
 import { OAuth2Client } from "google-auth-library/build/src/auth/oauth2client";
-import { google } from "googleapis";
-import queryString = require("querystring");
+import { google, people_v1 } from "googleapis";
+import { connect } from "net";
 import parseEnvironment from "./parse-environment";
 
+const { people } = google.people("v1");
+
 const GOOGLE_CONTACTS_SCOPE = "https://www.googleapis.com/auth/contacts.readonly";
-const GOOGLE_PEOPLE_CONNECTIONS_API = "https://people.googleapis.com/v1/people/me/connections";
 const RELEVANT_PHONE_NUMBER_TYPES = ["home", "work", "mobile"];
 
 const { clientId, clientSecret, redirectUrl } = parseEnvironment();
+
+const resourceName = "people/me";
+const personFields = ["metadata", "names", "emailAddresses", "organizations", "phoneNumbers"].join(
+	","
+);
+
+interface PhoneNumber {
+	label: string | null;
+	phoneNumber: string;
+}
 
 export function getOAuth2Client(): OAuth2Client {
 	return new google.auth.OAuth2(clientId, clientSecret, redirectUrl);
@@ -23,26 +33,112 @@ export function getOAuth2RedirectUrl(): string {
 	});
 }
 
-export async function getGoogleContacts(token: string): Promise<Contact[]> {
-	const query = queryString.stringify({
-		access_token: token,
-		personFields: ["names", "phoneNumbers"]
+export async function getGoogleContacts(client: OAuth2Client): Promise<Contact[]> {
+	const response = await people.connections.list({
+		auth: client,
+		personFields,
+		resourceName
 	});
-	const response = await axios(`${GOOGLE_PEOPLE_CONNECTIONS_API}?${query}`);
+
 	const { connections } = response.data;
-	return connections.map(convertGoogleConnection);
+
+	if (!Array.isArray(connections)) {
+		return [];
+	}
+
+	const contacts: Contact[] = [];
+
+	for (const connection of connections) {
+		const id = getGoogleContactId(connection);
+		const name = getGoogleContactName(connection);
+		const company = getGoogleContactCompany(connection);
+		const email = getGoogleContactPrimraryEmailAddress(connection);
+		const phoneNumbers = getGoogleContactPhoneNumbers(connection);
+
+		if (id && name && phoneNumbers) {
+			contacts.push({
+				id,
+				name,
+				email,
+				company,
+				phoneNumbers
+			});
+		}
+	}
+
+	return contacts;
 }
 
-export function convertGoogleConnection(connection: any): Contact {
-	const name = connection.names[0].displayName;
-	const phoneNumbers = connection.phoneNumbers
-		.filter(phoneNumber => RELEVANT_PHONE_NUMBER_TYPES.indexOf(phoneNumber.type) >= 0)
-		.map(phoneNumber => ({
-			label: phoneNumber.formattedType,
-			phoneNumber: phoneNumber.value
-		}));
-	return {
-		name,
-		phoneNumbers
-	};
+export function getGoogleContactId(connection: people_v1.Schema$Person): string | null {
+	const { metadata } = connection;
+	if (!metadata || !metadata.sources) {
+		return null;
+	}
+	const contactMetadata = metadata.sources.find(entry => entry.type === "CONTACT");
+	if (!contactMetadata || !contactMetadata.id) {
+		return null;
+	}
+	return contactMetadata.id;
+}
+
+export function getGoogleContactName(connection: people_v1.Schema$Person): string | null {
+	if (!connection.names) {
+		return null;
+	}
+	const [name] = connection.names;
+	if (!name) {
+		return null;
+	}
+	return name.displayName || null;
+}
+
+export function getGoogleContactCompany(connection: people_v1.Schema$Person): string | null {
+	if (!connection.organizations) {
+		return null;
+	}
+	const [company] = connection.organizations;
+	if (!company) {
+		return null;
+	}
+	return company.name || null;
+}
+
+export function getGoogleContactPhoneNumbers(
+	connection: people_v1.Schema$Person
+): PhoneNumber[] | null {
+	if (!connection.phoneNumbers) {
+		return null;
+	}
+	const relevantPhoneNumbers = connection.phoneNumbers.filter(
+		phoneNumber => phoneNumber.type && RELEVANT_PHONE_NUMBER_TYPES.indexOf(phoneNumber.type) >= 0
+	);
+	const phoneNumbers: PhoneNumber[] = [];
+	for (const phoneNumber of relevantPhoneNumbers) {
+		if (phoneNumber.value) {
+			phoneNumbers.push({
+				label: phoneNumber.formattedType || null,
+				phoneNumber: phoneNumber.value
+			});
+		}
+	}
+	if (phoneNumbers.length < 1) {
+		return null;
+	}
+	return phoneNumbers;
+}
+
+export function getGoogleContactPrimraryEmailAddress(
+	connection: people_v1.Schema$Person
+): string | null {
+	const { emailAddresses } = connection;
+	if (!emailAddresses) {
+		return null;
+	}
+	const primaryEmailAddress = emailAddresses.find(entry =>
+		Boolean(entry.metadata && entry.metadata.primary)
+	);
+	if (!primaryEmailAddress) {
+		return null;
+	}
+	return primaryEmailAddress.value || null;
 }
