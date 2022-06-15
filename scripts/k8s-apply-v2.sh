03e3e4821fb9970c3259f854770bcc48b64f7b6a
Leon Erath <erath@sipgate.de>
2022-06-15 15:51:07 +0200

    :recycle: Add v2 cloudbuild yaml

--- scripts/k8s-apply-v2.sh
+++ scripts/k8s-apply-v2.sh
@@ -4,10 +4,10 @@ set -e
 
 export PROJECT_ID="clinq-services"
 export GITHUB_SHA=$(git rev-parse --short HEAD)
-export APP="clinq-bridge-google"
+export APP="clinq-bridge-google-v2"
 export IMAGE="eu.gcr.io/$PROJECT_ID/$APP:latest"
-export DOMAIN="google.bridge.clinq.com"
-export GOOGLE_SECRET_NAME=" clinq-bridge-google"
+export DOMAIN="google.bridge.v2.clinq.com"
+export GOOGLE_SECRET_NAME=" clinq-bridge-google-v2"
 export REDIRECT_URI="https://$DOMAIN/oauth2/callback"
 
 kubectl kustomize k8s/template | envsubst > k8s/prod.yml
